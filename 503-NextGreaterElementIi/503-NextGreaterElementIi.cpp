9a59da350e1c54501298741dd96b01f3b5fb85dc
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:12:44 +0530

    Create 503-NextGreaterElementIi

---
+++ 503-NextGreaterElementIi/503-NextGreaterElementIi.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/24/2026, 10:12:44 PM
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = 2*n-1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums[i%n]) {
+                st.pop();
+            }
+            if(i<n){
+                ans[i]=st.empty()?-1:st.top();
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+        }
+};
