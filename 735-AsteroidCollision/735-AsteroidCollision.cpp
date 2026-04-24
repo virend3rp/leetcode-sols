583dec70e4259853c3faf8fc56d678bdd366430b
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:11:52 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -0,0 +1,35 @@
+// Last updated: 4/24/2026, 10:11:51 PM
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> stack;
+
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.push(a);
+            } else {
+                while (!stack.empty() && stack.top() > 0 && stack.top() < -a) {
+                    stack.pop();
+                }
+
+                if (stack.empty() || stack.top() < 0) {
+                    stack.push(a);
+                }
+
+                if (!stack.empty() && stack.top() == -a) {
+                    stack.pop();
+                }
+            }
+        }
+
+        vector<int> res(stack.size());
+        int i = stack.size() - 1;
+
+        while (!stack.empty()) {
+            res[i--] = stack.top();
+            stack.pop();
+        }
+
+        return res;        
+    }
+};
