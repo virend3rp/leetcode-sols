ceb588d4a648ba3529acb0b7272f290d37799cda
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:07:11 +0530

    Create 2216-DeleteTheMiddleNodeOfALinkedList

---
+++ 2216-DeleteTheMiddleNodeOfALinkedList/2216-DeleteTheMiddleNodeOfALinkedList.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/24/2026, 10:07:10 PM
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = prev->next->next;
+        slow->next = nullptr;
+        delete slow;
+        return head;
+    }
+};
