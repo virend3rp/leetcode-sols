8dba9ed84a82699430ea66b22f58a6707ecb2400
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:14:12 +0530

    Create 230-KthSmallestElementInABst

---
+++ 230-KthSmallestElementInABst/230-KthSmallestElementInABst.cpp
@@ -0,0 +1,28 @@
+// Last updated: 4/24/2026, 10:14:11 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void finorder(TreeNode* root,vector<int>& inorder){
+        if(root==NULL){
+            return;
+        }
+        finorder(root->left,inorder);
+        inorder.push_back(root->val);
+        finorder(root->right,inorder);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> inorder;
+        finorder(root,inorder);
+        return inorder[k-1];
+    }
+};
