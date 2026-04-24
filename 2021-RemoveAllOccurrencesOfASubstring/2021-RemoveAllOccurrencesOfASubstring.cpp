35d63a8921a51c6f5fd1de16fb5d6dfbae5df228
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:07:47 +0530

    Create 2021-RemoveAllOccurrencesOfASubstring

---
+++ 2021-RemoveAllOccurrencesOfASubstring/2021-RemoveAllOccurrencesOfASubstring.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/24/2026, 10:07:47 PM
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string resultStack;
+        int targetLength = part.size();
+        char targetEndChar = part.back();
+
+        for (char currentChar : s) {
+            resultStack.push_back(currentChar);
+
+            if (currentChar == targetEndChar && resultStack.size() >= targetLength) {
+                if (resultStack.substr(resultStack.size() - targetLength) == part) {
+                    resultStack.erase(resultStack.size() - targetLength);
+                }
+            }
+        }
+        return resultStack;
+    }
+}; 
