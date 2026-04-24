a6558bf081f591e2cc94bbc1561e02cbce807dea
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:15:24 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.cpp
@@ -0,0 +1,31 @@
+// Last updated: 4/24/2026, 10:15:24 PM
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int candy = n, i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
+            }
+            //For increasing slope
+            int peak = 0;
+            while(ratings[i] > ratings [i-1]){
+                peak++;
+                candy += peak;
+                i++;
+                if(i == n) return candy;
+            }
+            //For decreasing slope
+            int valley = 0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley); //Keep only the higher peak
+        }
+        return candy;
+    }
+};
