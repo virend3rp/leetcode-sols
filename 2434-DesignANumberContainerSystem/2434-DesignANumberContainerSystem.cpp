927135f808b7d5b8d92a87d8e678ec961703ee4c
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:06:34 +0530

    Create 2434-DesignANumberContainerSystem

---
+++ 2434-DesignANumberContainerSystem/2434-DesignANumberContainerSystem.cpp
@@ -0,0 +1,33 @@
+// Last updated: 4/24/2026, 10:06:33 PM
+class NumberContainers {
+public:
+    NumberContainers() {
+        
+    }
+        map<int,set<int>>mp;
+        map<int,int>id;
+         void change(int index, int number) {
+            if(!id.count(index))
+                id[index]=number,
+                mp[id[index]].insert(index);
+
+            else
+                mp[id[index]].erase(index),
+                id[index]=number,
+                mp[id[index]].insert(index); 
+        }
+        
+         int find(int number) {
+            if(!mp[number].size())
+                return -1;
+            
+            return *mp[number].begin();
+        }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
