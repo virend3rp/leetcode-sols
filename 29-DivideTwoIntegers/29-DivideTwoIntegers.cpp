70a7032587dea3c33ad0adcdd47f8c081d1a0c94
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:17:04 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/24/2026, 10:17:03 PM
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == divisor)
+            return 1;
+
+        bool isPositive = (dividend < 0) == (divisor < 0);
+
+        long long a = abs((long long)dividend);
+        long long b = abs((long long)divisor);
+        long long ans = 0;
+
+        while (a >= b) {
+            int q = 0;
+            while (a >= (b << (q + 1)))
+                q++;
+            ans += (1LL << q);
+            a -= (b << q);
+        }
+
+        if (ans > INT_MAX)
+            return isPositive ? INT_MAX : INT_MIN;
+
+        return isPositive ? ans : -ans;
+    }
+};
