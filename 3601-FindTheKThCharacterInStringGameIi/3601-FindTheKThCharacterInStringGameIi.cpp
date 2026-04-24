e7f6d9089b44e1957ef826fe7f815a4bfa437838
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:04:23 +0530

    Create 3601-FindTheKThCharacterInStringGameIi

---
+++ 3601-FindTheKThCharacterInStringGameIi/3601-FindTheKThCharacterInStringGameIi.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/24/2026, 10:04:22 PM
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        int shift = 0;
+        for (int i = (int)ceil(log2(k)) - 1; i >= 0; --i) {
+            long long half = 1LL << i;
+            if (k > half) {
+                k -= half;
+                shift += operations[i];
+            }
+        }
+        return 'a' + (shift % 26);
+    }
+};
