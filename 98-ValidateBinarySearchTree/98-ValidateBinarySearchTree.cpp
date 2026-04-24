0f74e161b45535f46aa2c4d90a3f070f9b41b0bf
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:16:03 +0530

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
@@ -0,0 +1,32 @@
+// Last updated: 4/24/2026, 10:16:02 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool is_bst(TreeNode *root , long mini , long maxi)
+    {
+        if(root == NULL)
+            return true;
+        
+        if(root->val >= maxi || root->val <= mini)
+            return false;
+
+        return is_bst(root->left , mini , root->val) && is_bst(root->right , root->val , maxi);
+    }
+
+    bool isValidBST(TreeNode* root) 
+    {
+        long mini = LONG_MIN;
+        long maxi = LONG_MAX;
+        return is_bst(root , mini , maxi);   
+    }
+};
