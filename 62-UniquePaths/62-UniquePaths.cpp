4cfa6b8f9fd5fabe09036fdccc72518d25c62722
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:16:31 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/24/2026, 10:16:30 PM
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                   int  up=0;
+                   int  left=0;
+                    if(i>0) left=dp[i-1][j];
+                    if(j>0) up=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
