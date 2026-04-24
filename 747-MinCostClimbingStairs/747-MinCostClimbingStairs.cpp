46d98e05a5c329b057bfcaea9ad7b0a4166d6bcd
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:11:50 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/24/2026, 10:11:49 PM
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1, 0); // We need an extra step beyond the last one
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n]; 
+    }
+};
