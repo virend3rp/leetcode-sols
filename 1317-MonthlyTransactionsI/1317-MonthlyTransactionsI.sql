2c21cf4afa9ff1abbcfadf90a279a4784edb0ab3
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:09:55 +0530

    Create 1317-MonthlyTransactionsI

---
+++ 1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,12 @@
+-- Last updated: 4/24/2026, 10:09:54 PM
+SELECT 
+    LEFT(trans_date, 7) AS month,
+    country, 
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    month, country;
