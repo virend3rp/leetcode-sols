9c3dfd7384a8a2d2184619efabeb235ccecb18d6
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:16:25 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/24/2026, 10:16:24 PM
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+
+        int start = 1;
+        int end = x;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else if (mid * mid == x)
+                return mid;
+            else
+                start = mid + 1;
+        }
+        return end;
+    }
+};
