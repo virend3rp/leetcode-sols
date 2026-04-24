2049b768a33d5a48f7969a36efb23b674024b051
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:04:15 +0530

    Create 3639-ZeroArrayTransformationI

---
+++ 3639-ZeroArrayTransformationI/3639-ZeroArrayTransformationI.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/24/2026, 10:04:15 PM
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> diff(nums.size() + 1, 0);
+        for (const auto& q : queries) {
+            diff[q[0]]++; // diff[l]++
+            if (q[1] + 1 < nums.size())
+                diff[q[1] + 1]--; // diff[r + 1]--
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            cnt += diff[i];
+            if (nums[i] > cnt)
+                return false;
+        }
+        return true;
+    }
+};
