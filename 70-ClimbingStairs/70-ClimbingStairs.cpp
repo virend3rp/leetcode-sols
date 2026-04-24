4f66e2c59c88c577cfe8a166e6cb8d12a642a57e
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:16:24 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/24/2026, 10:16:23 PM
+class Solution {
+public:
+unsigned int climbStairs(int n) {
+        int prev2 = 1;
+        int prev = 1;
+  
+    for(int i=2; i<=n; i++){
+        int cur_i = prev2+ prev;
+        prev2 = prev;
+        prev= cur_i;
+    }
+    return prev;
+    }
+};
