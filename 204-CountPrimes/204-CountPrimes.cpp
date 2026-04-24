ab2c39682f06c3a0738bac488b4f8f5b55c4389f
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:14:35 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/24/2026, 10:14:35 PM
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt = 0;
+        vector<bool> prime(n + 1, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                cnt++;
+                for (int j = i *2; j < n; j = j + i) {
+                    prime[j] = 0;
+                }
+            }
+        }
+        return cnt;
+    }
+};
