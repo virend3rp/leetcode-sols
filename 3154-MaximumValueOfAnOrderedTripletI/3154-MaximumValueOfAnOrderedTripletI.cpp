917ba56100e31d56b2a87a91001ae12d13bdf4ae
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:05:15 +0530

    Create 3154-MaximumValueOfAnOrderedTripletI

---
+++ 3154-MaximumValueOfAnOrderedTripletI/3154-MaximumValueOfAnOrderedTripletI.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/24/2026, 10:05:15 PM
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> leftMax(n), rightMax(n);
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - i]);
+        }
+        long long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = max(res, (long long)(leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
+    }
+};
