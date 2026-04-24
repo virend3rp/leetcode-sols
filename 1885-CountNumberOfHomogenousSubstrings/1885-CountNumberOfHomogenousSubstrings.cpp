b3a2e40d3b5e10c999006275cb64aa73dedcdaa6
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:08:09 +0530

    Create 1885-CountNumberOfHomogenousSubstrings

---
+++ 1885-CountNumberOfHomogenousSubstrings/1885-CountNumberOfHomogenousSubstrings.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/24/2026, 10:08:09 PM
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int left = 0;
+        long long res = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (s[left] == s[right]) {
+                res += right - left + 1;
+            } else {
+                res += 1;
+                left = right;
+            }
+        }
+
+        return (int) (res % (1000000007));        
+    }
+};
