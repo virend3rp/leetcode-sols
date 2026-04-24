1f66eef4e57a8fb0ed8e55e7f109da5ff620b61b
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:08:11 +0530

    Create 1882-TheNumberOfEmployeesWhichReportToEachEmployee

---
+++ 1882-TheNumberOfEmployeesWhichReportToEachEmployee/1882-TheNumberOfEmployeesWhichReportToEachEmployee.sql
@@ -0,0 +1,14 @@
+-- Last updated: 4/24/2026, 10:08:10 PM
+SELECT
+    e.employee_id,
+    e.name,
+    COUNT(r.employee_id) AS reports_count,
+    ROUND(AVG(r.age)) AS average_age
+FROM Employees e
+JOIN Employees r
+    ON r.reports_to = e.employee_id
+GROUP BY
+    e.employee_id,
+    e.name
+ORDER BY
+    e.employee_id;
