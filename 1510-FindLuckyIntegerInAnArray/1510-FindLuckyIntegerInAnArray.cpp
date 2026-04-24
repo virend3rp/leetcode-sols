864d87ad49fdc69e4c0ae15e4e4c715a8e981e62
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:09:11 +0530

    Create 1510-FindLuckyIntegerInAnArray

---
+++ 1510-FindLuckyIntegerInAnArray/1510-FindLuckyIntegerInAnArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 4/24/2026, 10:09:10 PM
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> freq;
+
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        int lucky = -1;
+
+        for (auto& [key, value] : freq) {
+            if (key == value) {
+                lucky = max(lucky, key);
+            }
+        }
+
+        return lucky;
+    }
+};
