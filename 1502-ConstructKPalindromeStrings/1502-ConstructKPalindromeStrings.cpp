0421eb3d1fbf6d99c394d78b05533a7f4aa9b229
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:09:14 +0530

    Create 1502-ConstructKPalindromeStrings

---
+++ 1502-ConstructKPalindromeStrings/1502-ConstructKPalindromeStrings.cpp
@@ -0,0 +1,12 @@
+// Last updated: 4/24/2026, 10:09:13 PM
+class Solution {
+public:
+    bool canConstruct(string& s, int k) {
+        const int n=s.size();
+        if (n<k) return 0;
+        bitset<26> freq=0;
+        for(char c: s)
+            freq.flip(c-'a');
+        return freq.count()<=k;
+    }
+};
