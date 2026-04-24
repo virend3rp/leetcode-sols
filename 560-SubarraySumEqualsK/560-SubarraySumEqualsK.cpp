462e92e0a954ddc4f8488ee064add6f8df68ae8f
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:12:31 +0530

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -0,0 +1,21 @@
+// Last updated: 4/24/2026, 10:12:30 PM
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> subNum;
+        subNum[0] = 1;
+        int total = 0, count = 0;
+
+        for (int n : nums) {
+            total += n;
+
+            if (subNum.find(total - k) != subNum.end()) {
+                count += subNum[total - k];
+            }
+
+            subNum[total]++;
+        }
+
+        return count;
+    }
+};
