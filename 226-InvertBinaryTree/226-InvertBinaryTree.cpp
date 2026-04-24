b7fbd688c3f554b46ccf35da93f66643e9512870
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:14:16 +0530

    Create 226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/226-InvertBinaryTree.cpp
@@ -0,0 +1,25 @@
+// Last updated: 4/24/2026, 10:14:15 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        TreeNode* temp=invertTree(root->left);
+        root->left=invertTree(root->right);
+        root->right=temp;
+
+        return root;
+    }
+};
