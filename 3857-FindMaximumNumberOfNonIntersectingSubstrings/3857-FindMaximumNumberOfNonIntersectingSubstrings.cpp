6ea47e1dab6ffda7ad7aff2ea839cf3295fa4be2
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:03:43 +0530

    Create 3857-FindMaximumNumberOfNonIntersectingSubstrings

---
+++ 3857-FindMaximumNumberOfNonIntersectingSubstrings/3857-FindMaximumNumberOfNonIntersectingSubstrings.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/24/2026, 10:03:43 PM
+class Solution {
+public:
+    int maxSubstrings(string word) {
+        int len = 0;
+        vector<int> arr(26, -1);
+        int n = word.length();
+        for(int i = 0; i < n; i++) {
+            int idx = word[i] - 'a';
+            if(arr[idx] != -1 && i - arr[idx] + 1 >= 4) {
+                len++;
+                for(int i = 0; i < 26; i++) {
+                    arr[i] = -1;
+                }
+            } else {
+                if(arr[idx] == -1) {
+                    arr[idx] = i;
+                }
+            }
+        }
+
+        return len;
+    }
+};
