529fda094743d6be8e9a947595add37cd8574d34
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:14:22 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/24/2026, 10:14:21 PM
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int val = nums[i];
+            if (seen.find(val) != seen.end() && i - seen[val] <= k) {
+                return true;
+            }
+            seen[val] = i;
+        }
+
+        return false;        
+    }
+};
