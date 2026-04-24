f4d79775a9d97cd08cd21f672d0989e48feb35c4
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:04:44 +0530

    Create 3430-CountDaysWithoutMeetings

---
+++ 3430-CountDaysWithoutMeetings/3430-CountDaysWithoutMeetings.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/24/2026, 10:04:43 PM
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+
+        vector<vector<int>> mergedMeetings;
+        for (const auto& meeting : meetings) {
+            if (mergedMeetings.empty() || meeting[0] > mergedMeetings.back()[1]) {
+                mergedMeetings.push_back(meeting);
+            } else {
+                mergedMeetings.back()[1] = max(mergedMeetings.back()[1], meeting[1]);
+            }
+        }
+
+        int meetingDaysCount = 0;
+        for (const auto& m : mergedMeetings) {
+            meetingDaysCount += (m[1] - m[0] + 1);
+        }
+
+        return days - meetingDaysCount;
+    }
+};
