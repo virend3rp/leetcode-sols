058781fc3d33485ce7d17c504234df69cb0e7c64
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:13:10 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.cpp
@@ -0,0 +1,37 @@
+// Last updated: 4/24/2026, 10:13:09 PM
+class Solution {
+public:
+ int thirdMax(vector<int>& nums) {
+        if(nums.size()==1){           // when size of Array is 1
+         return nums[0];   
+        }
+        if(nums.size()==2){           // When size of array is 2
+            return max(nums[0],nums[1]);
+        }
+ // find First maximum element using *max_element() function
+        int First_max=*max_element(nums.begin(),nums.end());   
+	    int Second_max=INT_MIN;
+        int Third_max=INT_MIN;
+        int count1=0;
+        for(int i=0;i<nums.size();i++){
+            if(Second_max<nums[i] && nums[i]!=First_max){
+                Second_max=nums[i];
+                count1++;
+            }
+        }
+// if Second maximum element does not exist return First maximum element
+        if(count1==0) return First_max; 
+        int count2=0;
+        for(int i=0;i<nums.size();i++){
+            if((Third_max<nums[i] || nums[i]==INT_MIN) && ( nums[i]!=First_max &&  nums[i]!=Second_max)){
+                Third_max=nums[i];
+                count2++;
+            }
+        }
+        if(count2>0){
+            return Third_max;
+        }else{
+            return First_max;
+        }
+    }
+};
