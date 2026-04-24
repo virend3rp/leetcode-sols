ce59630bb6e5d7d46dee146fe9830a07fb98b577
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:15:33 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/24/2026, 10:15:33 PM
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length()-1;
+        while(left<right)
+        {
+            if(!isalnum(s[left])) 
+                left++;
+            else if(!isalnum(s[right])) 
+                right--;
+            else if(tolower(s[left])!=tolower(s[right])) 
+                return false;
+            else {
+                left++; 
+                right--;
+            }
+        }
+        return true;
+
+    }
+};
