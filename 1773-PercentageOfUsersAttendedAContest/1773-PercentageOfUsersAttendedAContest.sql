ed367ef2457938d9dfb228025470120e31d944ff
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:08:29 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,10 @@
+-- Last updated: 4/24/2026, 10:08:28 PM
+SELECT 
+    r.contest_id,
+    ROUND(
+        COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 
+        2
+    ) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, contest_id ASC;
