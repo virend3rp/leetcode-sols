ffd929c1dc0967510e154f0b744f60d9c1263246
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:06:51 +0530

    Create 2299-MergeNodesInBetweenZeros

---
+++ 2299-MergeNodesInBetweenZeros/2299-MergeNodesInBetweenZeros.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/24/2026, 10:06:51 PM
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* dummy = new ListNode();
+        ListNode* cur = dummy;
+        head = head->next;
+        int sum = 0;
+        while (head != nullptr) {
+            if (head->val == 0) {
+                cur->next = new ListNode(sum);
+                cur = cur->next;
+                sum = 0;
+            }
+            sum += head->val;
+            head = head->next;
+        }
+        return dummy->next;
+    }
+};
