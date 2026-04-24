1ae652fa9be45fbf13dbfe39a49fbb651393b4bd
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:05:07 +0530

    Create 3201-DistributeCandiesAmongChildrenIi

---
+++ 3201-DistributeCandiesAmongChildrenIi/3201-DistributeCandiesAmongChildrenIi.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/24/2026, 10:05:07 PM
+class Solution {
+public:
+    long long combCount(int sum) {
+        if (sum < 0) return 0;
+        return 1LL * (sum + 2) * (sum + 1) / 2;
+    }
+
+    long long distributeCandies(int n, int limit) {
+        return combCount(n)
+             - 3 * combCount(n - (limit + 1))
+             + 3 * combCount(n - 2 * (limit + 1))
+             - combCount(n - 3 * (limit + 1));
+    }
+};
