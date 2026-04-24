40fd453d081f3465179c4ec9d2c8561a5e690452
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:10:03 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/24/2026, 10:10:02 PM
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        // Store the rank for each number in arr
+        unordered_map<int, int> numToRank;
+        // Deduplicate and sort arr
+        set<int> nums(arr.begin(), arr.end());
+        int rank = 1;
+        for (auto num : nums) {
+            numToRank[num] = rank;
+            rank++;
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = numToRank[arr[i]];
+        }
+        return arr;
+    }
+};
