9fc0a1ada9af0931ea045b1209fbc5f74e39acf6
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:14:31 +0530

    Create 208-ImplementTriePrefixTree

---
+++ 208-ImplementTriePrefixTree/208-ImplementTriePrefixTree.cpp
@@ -0,0 +1,68 @@
+// Last updated: 4/24/2026, 10:14:30 PM
+struct Node {
+    Node* links[26];
+    bool flag = false;
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    void setEnd() {
+        flag = true;
+    }
+    bool isEnd() {
+        return flag;
+    }
+};
+
+// Trie class
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+    void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        // Mark the end of the word
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                return false;
+            }
+            // Move to the next node
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (!node->containsKey(prefix[i])) {
+                // If a letter is not found, there is
+                // no word with the given prefix
+                return false;
+            }
+            // Move to the next node
+            node = node->get(prefix[i]);
+        }
+        // The prefix is found in the Trie
+        return true;
+    }
+};
