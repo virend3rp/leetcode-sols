530a179e58f5e651a43f7c5fa9fedee5a97b8b92
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-24 22:08:28 +0530

    Create 1777-DetermineIfTwoStringsAreClose

---
+++ 1777-DetermineIfTwoStringsAreClose/1777-DetermineIfTwoStringsAreClose.cpp
@@ -0,0 +1,35 @@
+// Last updated: 4/24/2026, 10:08:27 PM
+class Solution {
+public:
+    bool closeStrings(std::string word1, std::string word2) {
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for (char ch : word1) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2) {
+            freq2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
