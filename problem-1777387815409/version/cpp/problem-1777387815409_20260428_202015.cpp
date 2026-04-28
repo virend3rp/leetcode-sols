d15ea08aa1f1cb9ea28e0acaed7eed4b1a6fac0b
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-04-28 20:20:16 +0530

    Create

---
+++ problem-1777387815409/version/cpp/problem-1777387815409_20260428_202015.cpp
@@ -0,0 +1,29 @@
+// Last updated: 4/28/2026, 8:20:15 PM
+1class Solution {
+2public:
+3int nextValid(string& s, int i) {
+4    int skip = 0;
+5    while (i >= 0) {
+6        if (s[i] == '#') { skip++; i--; }
+7        else if (skip > 0) { skip--; i--; }
+8        else break;
+9    }
+10    return i;
+11}
+12
+13bool backspaceCompare(string s, string t) {
+14    int i = s.size() - 1;
+15    int j = t.size() - 1;
+16
+17    while (i >= 0 || j >= 0) {
+18        i = nextValid(s, i);
+19        j = nextValid(t, j);
+20
+21        if (i >= 0 && j >= 0 && s[i] != t[j]) return false;
+22        if ((i >= 0) != (j >= 0)) return false;
+23
+24        i--; j--;
+25    }
+26    return true;
+27}
+28};
