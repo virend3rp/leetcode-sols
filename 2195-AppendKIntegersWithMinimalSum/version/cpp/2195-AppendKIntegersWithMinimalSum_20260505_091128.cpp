86918d49b416d28c0c95aa4a1c55d74d12517984
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2026-05-05 09:11:29 +0530

    Create 2195-AppendKIntegersWithMinimalSum

---
+++ 2195-AppendKIntegersWithMinimalSum/version/cpp/2195-AppendKIntegersWithMinimalSum_20260505_091128.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/5/2026, 9:11:28 AM
+1class Solution {
+2public:
+3    long long minimalKSum(vector<int>& nums, int k) {
+4        set<int> s(nums.begin(), nums.end());
+5        long long ans = 0, next = 1;
+6        for (int num : s) {
+7            if (k == 0) break;
+8            if (num > next) {
+9                long long fill = min((long long)k, (long long)(num - next));
+10                ans += (next + next + fill - 1) * fill / 2;
+11                k -= fill;
+12            }
+13            next = num + 1;
+14        }
+15        if (k > 0) ans += (next + next + k - 1LL) * k / 2;
+16        return ans;
+17    }
+18};
